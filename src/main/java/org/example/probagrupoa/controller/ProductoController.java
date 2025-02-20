5aec8293c113e2adfb8b7d71b4d888ebd8d27dbb
VIZ3NT04 <vicent.ceip.rius@gmail.com>
2025-02-20 13:16:27 +0100

    ACABAT

--- src/main/java/org/example/probagrupoa/controller/ProductoController.java
+++ src/main/java/org/example/probagrupoa/controller/ProductoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("s/productos")
+@RequestMapping("/productos")
 public class ProductoController {
 
     @Autowired
